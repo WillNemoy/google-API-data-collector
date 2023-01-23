2d2650e11c7f8b6f33ecf5907f868ec5012043e3
Will Nemoy <98560701+WillNemoy@users.noreply.github.com>
2023-01-22 22:04:49 -0500

    Created the reviews collector

--- app.py
+++ app.py
@@ -9,9 +9,53 @@ import numpy as np
 
 load_dotenv() #look in the ".env" file for env vars
 
+def googleAPI_places(GOOGLE_API_KEY_parameter, search_term): #, location_parameter):
 
-def googleAPI(YELP_API_KEY_parameter, search_term, location_parameter):
+    # Python program to get a set of 
+    # places according to your search 
+    # query using Google Places API
     
+    # https://developers.google.com/maps/documentation/places/web-service/search-text    
+    response = requests.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" 
+                            + search_term 
+                            + '&key=' 
+                            + GOOGLE_API_KEY_parameter
+                            + '&key=')
+    
+    data = response.json()
+
+    return data
+
+# https://developers.google.com/maps/documentation/places/web-service/place-data-fields#places-api-fields-support
+def googleAPI_reviews(GOOGLE_API_KEY_parameter, place_id_parameter):
+
+    response = requests.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=" 
+                            + place_id_parameter 
+                            + '&key=' 
+                            + GOOGLE_API_KEY_parameter)
+    data = response.json()
+
+    return data
+        
     
 
+#run the function
 
+"""
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+my_place_id = "ChIJRfH9lXW7woARDUjiE-YILn8"
+
+my_reviews_data = googleAPI_reviews(GOOGLE_API_KEY, my_place_id)
+
+pprint(my_reviews_data)
+"""
+
+user_search_term = input("What type of businesses are you interested in? ")
+user_location = input("What area should we focus on? ")
+
+user_input = user_search_term + " " + user_location
+
+my_data = googleAPI_places(GOOGLE_API_KEY, user_input)
+
+pprint(my_data)
