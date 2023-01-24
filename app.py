03a9f7850b461ec9bbec7a902ca2ae0a6c055005
Will Nemoy <98560701+WillNemoy@users.noreply.github.com>
2023-01-23 23:45:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ import requests
 import pandas as pd
 import numpy as np
 
-load_dotenv() #look in the ".env" file for env vars
+from spacy.lang.en import English
+from spacy.lang.en.stop_words import STOP_WORDS
 
-def googleAPI_places(GOOGLE_API_KEY_parameter, search_term): #, location_parameter):
+
+def googleAPI(GOOGLE_API_KEY_parameter, search_term): #, location_parameter):
 
     # Python program to get a set of 
     # places according to your search 
@@ -23,41 +25,31 @@ def googleAPI_places(GOOGLE_API_KEY_parameter, search_term): #, location_paramet
                             + '&key=')
     
     data = response.json()
+    all_reviews = []
+
+    for place in places_data:
+        place_review_data = 
+        response = requests.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=" 
+                            + place_id_parameter 
+                            + '&key=' 
+                            + GOOGLE_API_KEY_parameter)
+        all_reviews.append(response.json())
 
     return data
 
 # https://developers.google.com/maps/documentation/places/web-service/place-data-fields#places-api-fields-support
-def googleAPI_reviews(GOOGLE_API_KEY_parameter, place_id_parameter):
 
-    response = requests.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=" 
-                            + place_id_parameter 
-                            + '&key=' 
-                            + GOOGLE_API_KEY_parameter)
-    data = response.json()
-
-    return data
         
-    
 
 #run the function
+load_dotenv() #look in the ".env" file for env vars
 
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-
-my_place_id = "ChIJRfH9lXW7woARDUjiE-YILn8"
-
-my_reviews_data = googleAPI_reviews(GOOGLE_API_KEY, my_place_id)
-
-pprint(my_reviews_data)
-
-
-"""
 user_search_term = input("What type of businesses are you interested in? ")
 user_location = input("What area should we focus on? ")
 
 user_input = user_search_term + " " + user_location
 
-my_data = googleAPI_places(GOOGLE_API_KEY, user_input)
+my_data = googleAPI(GOOGLE_API_KEY, user_input)
 
 pprint(my_data)
-"""
